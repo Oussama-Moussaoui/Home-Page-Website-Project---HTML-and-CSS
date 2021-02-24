69d5b86cb2b9f9aabc95d0988c89c5c1ae42a7e1
unknown <oussamamoussaoui.business@gmail.com>
2021-02-24 18:28:11 +0100

    Added footer, and smooth scrolling

--- js/app.js
+++ js/app.js
@@ -14,3 +14,30 @@ function initMap() {
 
     const marker = new google.maps.Marker({ position: loc, map:map});
 }
+
+// Sticky Navbar Opacity
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector("#navbar");
+    if (window.scrollY > 150) {
+        navbar.style.opacity = 0.85;
+    } else {
+        navbar.style.opacity = 1;
+    }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 60
+        },
+        800
+      );
+    }
+  });
