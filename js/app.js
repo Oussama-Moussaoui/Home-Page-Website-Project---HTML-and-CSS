84b17d1b290474db985716c521aef5b7d027b346
unknown <oussamamoussaoui.business@gmail.com>
2021-02-24 22:12:01 +0100

    Making it look good on Smartphones

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ $('#navbar a, .btn').on('click', function (event) {
         const hash = this.hash;
 
         $('html, body').animate({
-                scrollTop: $(hash).offset().top - 60
+                scrollTop: $(hash).offset().top - 100
             },
             800
         );
